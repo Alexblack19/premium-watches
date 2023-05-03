820cfe7d0dad4761dc0fe5b71260ca6ee1c9a659
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-05-03 14:30:15 +0300

    add files js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,4 +1,3 @@
-// Mobile menu start//
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
@@ -27,23 +26,3 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
-// Mobile menu end//
-
-// Modal start//
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
-// Modal end//
-
-
