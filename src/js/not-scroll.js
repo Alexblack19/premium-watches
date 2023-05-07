ede230edc6f75a101579fc158efcba5714c7570e
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-05-07 17:34:33 +0300

    add not-scroll.js

---
+++ src/js/not-scroll.js
@@ -0,0 +1,13 @@
+// «Prevent Page Scrolling When a Modal is Open»
+
+// When the modal is shown, we want a fixed body
+document.body.style.position = 'fixed';
+document.body.style.top = `-${window.scrollY}px`;
+
+// When the modal is hidden, we want to remain at the top of the scroll position
+document.body.style.position = '';
+document.body.style.top = '';
+
+// Когда модальное окно скрыто...
+const scrollY = document.body.style.top;
+document.body.style.position = '';
