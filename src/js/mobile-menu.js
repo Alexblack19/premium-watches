d73687c3c3cb83ac8903ff04997cebd1cc07b7e8
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-05-07 18:59:16 +0300

    update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,9 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  // =================================================================
+  const closeMenuLink = document.querySelector('.js-close-menu-link');
+  // =================================================================
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,6 +20,9 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  // ================================================
+  closeMenuLink.addEventListener('click', toggleMenu);
+  // ================================================
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
