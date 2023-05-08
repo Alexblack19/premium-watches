87009332641db9fb70cad815a8e47f1865aec5de
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-05-09 00:33:07 +0300

    Update not-scroll.js

--- src/js/not-scroll.js
+++ src/js/not-scroll.js
@@ -11,3 +11,5 @@ document.body.style.top = '';
 // Когда модальное окно скрыто...
 const scrollY = document.body.style.top;
 document.body.style.position = '';
+document.body.style.top = '';
+window.scrollTo(0, parseInt(scrollY || '0') * -1);
